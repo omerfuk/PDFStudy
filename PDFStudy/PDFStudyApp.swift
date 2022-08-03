20f7b01592b538970dfd4e951b8e37e87eb57e1c
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-08-03 11:34:14 +0300

    Initial Commit

---
+++ PDFStudy/PDFStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFStudyApp.swift
+//  PDFStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 3.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PDFStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
